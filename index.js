ac7fe8e0827212d44f03eddf98a1d9c1327dfb59
Jenn <>
2021-05-18 01:09:20 +0000

    initialized each variable coded the report

--- index.js
+++ index.js
@@ -2,13 +2,62 @@
 
 
 // Declare and initialize the 12 variables here:
-
-
+let date = 'Monday 2019-03-18';
+let time = '10:05:34 AM';
+let astronautCount = 7;
+let astronautStatus = 'ready';
+let averageAstronautMassKg = 80.7;
+let crewMassKg = astronautCount * averageAstronautMassKg;
+let fuelMassKg = 760000;
+let shuttleMassKg = 74842.31;
+let totalMassKg = crewMassKg + fuelMassKg + shuttleMassKg;
+let fuelTempCelsius = -255;
+let fuelLevel = '100%';
+let weatherStatus = 'clear';
+let dash = '-------------------------------------'
 
 
 // Write code to generate the LC04 report here:
+console.log(dash);
+console.log('> LC04 - LAUNCH CHECKLIST')
+console.log(dash);
+console.log('Date: ' + date);
+console.log('Time: ' + time);
+console.log(' ');
 
+console.log(dash);
+console.log('> ASTRONAUT INFO');
+console.log(dash);
+console.log('count: ' + astronautCount);
+console.log('status: ' + astronautCount);
+console.log(' ');
 
+console.log(dash);
+console.log('> FUEL DATA');
+console.log(dash);
+console.log('Fuel temp celsius: ' + fuelTempCelsius);
+console.log('Fuel level: ' + fuelLevel);
+console.log(' ');
+
+console.log(dash);
+console.log('> MASS DATA');
+console.log(dash);
+console.log('Crew mass: ' + crewMassKg);
+console.log('Fuel mass: ' + fuelMassKg);
+console.log('Shuttle mass: ' + shuttleMassKg);
+console.log('Total mass: ' + totalMassKg);
+console.log(' ');
+
+console.log(dash);
+console.log('> FLIGHT PLAN');
+console.log(dash);
+console.log('weather: ' + weatherStatus);
+console.log(' ');
+
+console.log(dash);
+console.log('> OVERALL STATUS');
+console.log(dash);
+console.log('Clear for takeoff: YES');
 
 
 // When done, have your TA check your code.
